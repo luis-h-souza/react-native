3de3493095ed8b8f103d8a324ece2dc4a405d028
Luis Henrique <luis.h.souza@outlook.com.br>
2025-06-22 21:57:50 -0300

    textInput testes

--- template/App.js
+++ template/App.js
@@ -1,12 +1,46 @@
+import clsx from 'clsx';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-web';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [isValidEmail, setIsValidEmail] = useState(true)
+
+  const handleEmailChange =(text) => {
+    setEmail(text)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    setIsValidEmail(emailRegex.test(text))
+  }
+  const handlePhoneChange =(text) => {
+    setPhone(text)
+  }
+
   return (
     <View style={formatacao.container}>
-      <Text style={formatacao.titulo}>My first App!</Text>
-      <Button style={formatacao.btn}>clicar aqui!</Button>
+      <Text style={formatacao.titulo}>Formulário</Text>
+
+      <TextInput
+        style={[formatacao.input, !isValidEmail && formatacao.inputInvalid]}
+        value={email}
+        onChangeText={handleEmailChange}
+        placeholder='Digite seu email'
+        keyboardType='email-address'
+      />
+
+      <TextInput 
+        style={[formatacao.input, !isValidEmail && formatacao.inputInvalid]}
+        value={phone}
+        onChangeText={handlePhoneChange}
+        placeholder='Digite seu telefone'
+        keyboardType='phone-pad'
+      />
+
+      <Pressable style={formatacao.btn}>
+        <Text style={formatacao.btnText}>Entrar</Text>
+      </Pressable >
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,17 +49,41 @@ export default function App() {
 const formatacao = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#242424',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#d565'
   },
   titulo: {
-    color: '#f4f4f4',
     fontWeight: 'bold',
-    fontSize: 22,
+    fontSize: 24,
+    marginBottom: 24
   },
   btn: {
-    backgroundColor: '#f4f4f4',
-    padding: '16px',
+    backgroundColor: '#b5b5',
+    padding: 24,
+    width: 300,
+    borderRadius: 8,
+    marginBlock:24,
+  },
+  btnText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textTransform: 'uppercase',
+  },
+  input: {
+    height: 56,
+    width: 300,
+    borderColor: '#000',
+    borderWidth: 1,
+    padding: 16,
+    marginBlock: 12,
+    backgroundColor: '#fffff',
+    borderRadius: 8,
+    marginVertical: 24
+  },
+  inputInvalid: {
+    borderColor: 'red',
   },
 });
